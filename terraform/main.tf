625f1192bc7c778b02c31d7485cc6cc35ab87e5f
Jakob Lingel <jlingel68@gmail.com>
2025-06-07 18:00:48 +0200

    Add hetzner cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.44"
+    }
   }
 }
 
@@ -11,15 +15,33 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
-module "dns" {
- source = "./global/dns"
- droplet_ipv4 = digitalocean_droplet.digitalocean_droplet_1.ipv4_address
+provider "hcloud" {
+  token = var.hcloud_token
 }
 
-resource "digitalocean_droplet" "digitalocean_droplet_1" {
-  name   = "digitalocean_droplet_1"
+module "dns" {
+ source = "./global/dns"
+ droplet_ipv4 = digitalocean_droplet.digitalocean-droplet-1.ipv4_address
+}
+
+resource "digitalocean_droplet" "digitalocean-droplet-1" {
+  name   = "digitalocean-droplet-1"
   region = "fra1"
   size   = "s-1vcpu-1gb"
   image  = "ubuntu-22-04-x64"
+}
+
+resource "hcloud_server" "web" {
+  name        = "hetzner-server"
+  image       = "ubuntu-22.04"
+  server_type = "cx11"
+  location    = "nbg1"
+
+  ssh_keys = [hcloud_ssh_key.default.id]
+}
+
+resource "hcloud_ssh_key" "default" {
+  name       = "main-key"
+  public_key = file(var.hcloud_ssh_key_path)
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,14 @@ variable "digitalocean_token" {
   sensitive   = true
 }
 
+variable "hcloud_token" {
+  type        = string
+  description = "Hetzner Cloud API token"
+  sensitive   = true
+}
+
+variable "hcloud_ssh_key_path" {
+  type        = string
+  description = "Path to the SSH key for Hetzner Cloud"
+  sensitive   = true
+}
