864df5ec743c3cdb37370b6fc3db1e8355d8af90
Jakob Lingel <jlingel68@gmail.com>
2025-06-15 15:17:56 +0200

    SSH credentials generation

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,9 +39,10 @@ module "dns" {
   portfolio_netlify_challenge_txt                           = var.portfolio_netlify_challenge_txt
 }
 
-module "schluesselmomente" {
+module "projects" {
   source                      = "./projects"
   hetzner_cloud_server_1_ipv4 = var.hetzner_cloud_server_1_ipv4
+  hetzner_cloud_server_1_deployment_private_ssh_key = tls_private_key.hetzner_private_key.private_key_pem
 }
 
 # compute
@@ -65,4 +66,20 @@ resource "hcloud_server" "hetzner_ubuntu-4gb-nbg1-1" {
 resource "hcloud_ssh_key" "hetzner_ssh_key" {
   name       = "hetzner-1"
   public_key = var.hcloud_ssh_key
+}
+
+resource "tls_private_key" "hetzner_private_key" {
+  algorithm = "ED25519"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "hcloud_ssh_key" "deployment_key" {
+  name       = "deployment-key"
+  public_key = tls_private_key.hetzner_private_key.public_key_openssh
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
--- terraform/projects/variables.tf
+++ terraform/projects/variables.tf
@@ -3,3 +3,9 @@ variable "hetzner_cloud_server_1_ipv4" {
   description = "IP address of the first Hetzner cloud server"
   sensitive   = true
 }
+
+variable "hetzner_cloud_server_1_deployment_private_ssh_key" {
+  type        = string
+  description = "Private SSH key for deployment of the first Hetzner cloud server"
+  sensitive   = true
+}
