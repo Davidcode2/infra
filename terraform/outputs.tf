dac08d7b97949a5d6e1d31bf7688ad655a653f7e
Jakob Lingel <jlingel68@gmail.com>
2025-11-02 21:23:21 +0100

    Fix node attribute issues

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,16 +5,21 @@ output "load_balancer_ip" {
 
 # This output makes the server private IPs available
 output "server_private_ips" {
-  value = hcloud_server.k8s_node.*.ipv4 # Use .ipv4 (public) or .private_net[0].ip (private)
+  value = hcloud_server.k8s_node.*.ipv4_address # Use .ipv4 (public) or .private_net[0].ip (private)
 }
 
 # This resource generates the inventory.ini file
 resource "local_file" "ansible_inventory" {
   # The content comes from the template file
-  content = templatefile("${path.module}/../ansible/inventory.ini.tftpl", {
+  content = templatefile("${path.module}/../ansible/inventory/inventory.ini.tfpl", {
     # Pass Terraform data into the template
-    lb_ip     = hcloud_load_balancer.k8s_lb.ipv4
-    masters   = hcloud_server.k8s_node
+    lb_ip   = hcloud_load_balancer.k8s_lb.ipv4
+    masters = [
+      for s in hcloud_server.k8s_node : {
+        name = s.name
+        ipv4 = s.ipv4_address
+      }
+    ]
     ssh_key_path = var.ssh_private_key_path # Add a variable for this
   })
 
