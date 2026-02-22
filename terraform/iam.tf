e68c42f438128e953c29bb5d0dc70f6eb22d1f14
Jakob Lingel <jlingel68@gmail.com>
2026-02-22 20:27:38 +0100

    Add teachme ssm permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -45,7 +45,8 @@ resource "aws_iam_role_policy" "ci-policy" {
           "arn:aws:ssm:eu-central-1:${var.aws_account_id}:parameter/compute/*",
           "arn:aws:ssm:eu-central-1:${var.aws_account_id}:parameter/immoly*",
           "arn:aws:ssm:eu-central-1:${var.aws_account_id}:parameter/joy_alemazung*",
-          "arn:aws:ssm:eu-central-1:${var.aws_account_id}:parameter/home_at_sea*"
+          "arn:aws:ssm:eu-central-1:${var.aws_account_id}:parameter/home_at_sea*",
+          "arn:aws:ssm:eu-central-1:${var.aws_account_id}:parameter/teachme*"
         ]
       }
     ]
@@ -83,7 +84,8 @@ resource "aws_iam_user_policy" "external_secrets_ssm" {
           "arn:aws:ssm:eu-central-1:${data.aws_caller_identity.current.account_id}:parameter/joy_alemazung/*",
           "arn:aws:ssm:eu-central-1:${data.aws_caller_identity.current.account_id}:parameter/schluesselmomente/*",
           "arn:aws:ssm:eu-central-1:${data.aws_caller_identity.current.account_id}:parameter/umami/*",
-          "arn:aws:ssm:eu-central-1:${data.aws_caller_identity.current.account_id}:parameter/home_at_sea/*"
+          "arn:aws:ssm:eu-central-1:${data.aws_caller_identity.current.account_id}:parameter/home_at_sea/*",
+          "arn:aws:ssm:eu-central-1:${data.aws_caller_identity.current.account_id}:parameter/teachme*"
         ]
       },
       {
