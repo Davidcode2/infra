3d590a3bfcfe4d19c13bbe118f85db113777e703
Jakob Lingel <jlingel68@gmail.com>
2026-03-16 20:44:22 +0100

    feat: Add AWS SSM parameters for message-router
    - /message-router/resend-api-key (SecureString)
    - /message-router/api-key (SecureString)
    - /message-router/from-email (String)

---
+++ terraform/projects/message-router_ssm.tf
@@ -0,0 +1,33 @@
+#################
+# message-router#
+#################
+
+resource "aws_ssm_parameter" "message_router_resend_api_key" {
+  name        = "/message-router/resend-api-key"
+  description = "Resend API key for message-router"
+  type        = "SecureString"
+  value       = "dummy"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "message_router_api_key" {
+  name        = "/message-router/api-key"
+  description = "API key for message-router frontend authentication"
+  type        = "SecureString"
+  value       = "dummy"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "message_router_from_email" {
+  name        = "/message-router/from-email"
+  description = "Sender email address for message-router"
+  type        = "String"
+  value       = "noreply@jakob-lingel.dev"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
