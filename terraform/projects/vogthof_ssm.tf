3c5fdf31ab0918e2874b30e77b087589c11c51f4
Jakob Lingel <jlingel68@gmail.com>
2026-04-10 23:28:52 +0200

    Update email ssm type

--- terraform/projects/vogthof_ssm.tf
+++ terraform/projects/vogthof_ssm.tf
@@ -5,7 +5,7 @@
 resource "aws_ssm_parameter" "vogthof_recipient_email" {
   name        = "/vogthof/notifications/recipient_email_address"
   description = "Recipient email address for vogthof form submissions"
-  type        = "String"
+  type        = "SecureString"
   value       = "dummy"
   lifecycle {
     ignore_changes = [value]
